6c6e4f252f4e040def53618ec33e0a88df56ceeb
norcon <w7654e@gmail.com>
2018-08-11 20:32:32 -0400

    Update working

--- src/econosim.rs
+++ src/econosim.rs
@@ -1,67 +1,147 @@
+#![allow(non_snake_case)]
 pub mod start_econ {
-use std::io::prelude::*;
-use std::io::BufReader;
+    use serde_json::to_string;
+    use serde_json::*;
     use std::error::Error;
     use std::fs::File;
+    use std::io::prelude::*;
+    use std::io::BufReader;
     use std::path::Path;
-    use serde_json::*;
-    use serde_json::to_string;
 
-
-#[derive(Debug, Serialize, Deserialize)]
-    pub enum ptype { 
+    #[derive(Debug, Serialize, Deserialize)]
+    pub enum ptype {
         large,
-            meduim,
-            small,
+        meduim,
+        small,
     }
 
-#[derive(Debug, Serialize, Deserialize)]
+    #[derive(Debug, Serialize, Deserialize)]
     pub enum goods {
         sugar(u64),
-            ice(u64),
-            cotton(u64),
-            gold(u64),
-            iron(u64),
+        ice(u64),
+        cotton(u64),
+        gold(u64),
+        iron(u64),
     }
 
-#[derive(Debug, Serialize, Deserialize)]
+    #[derive(Debug, Serialize, Deserialize)]
     pub struct port {
         pub name: String,
-            pub sizeof: ptype,
-            pub economy: Vec<goods>,
-            pub route: Vec<usize>
+        pub sizeof: ptype,
+        pub economy: Vec<goods>,
+        pub route: Vec<usize>,
     }
 
-    pub fn bootstrap(filename: String) -> Vec<port>{
+    pub fn bootstrap(filename: String) -> Vec<port> {
         let mut places = vec![];
         let path = Path::new(&*filename);
         let display = path.display();
         let mut f = match File::open(&path) {
             Err(why) => panic!("No"),
-                Ok(file) => file,
+            Ok(file) => file,
         };
         let mut s = String::new();
         let file = BufReader::new(&f);
         for (num, line) in file.lines().enumerate() {
             let l = line.unwrap();
-            let line: String = l.chars().skip(1).collect();
-            places.push(from_str(&*line).unwrap());
-        }  
+            let line: String = l.chars().collect();
+            places.push(from_str(&line).unwrap());
+        }
 
         places
     }
 
     pub fn get_js() -> String {
-        let mut toj: port = port {name: "Port Royal".to_string(), sizeof: ptype::large , economy: vec![goods::iron(1), goods::sugar(100), goods::cotton(50)], route: vec![0,1]};
+        let mut toj: port = port {
+            name: "Port Royal".to_string(),
+            sizeof: ptype::large,
+            economy: vec![goods::iron(1), goods::sugar(100), goods::cotton(50)],
+            route: vec![0, 1],
+        };
         let s = to_string(&toj);
         s.unwrap()
     }
-}
 
+    impl goods {
+        fn same(&self, other: &goods) -> bool {
+            let x = match &self {
+                goods::sugar(u64) => 1,
+                goods::ice(u64) => 2,
+                goods::cotton(u64) => 3,
+                goods::gold(u64) => 4,
+                goods::iron(u64) => 5,
+            };
+            let y = match other {
+                goods::sugar(u64) => 1,
+                goods::ice(u64) => 2,
+                goods::cotton(u64) => 3,
+                goods::gold(u64) => 4,
+                goods::iron(u64) => 5,
+            };
+            let mut ret = false;
+            if x == y {
+                ret = true;
+            }
+            ret
+        }
+        pub fn getVal(&self) -> u64 {
+            match &self {
+                goods::sugar(val) => val + 0,
+                goods::ice(val) => val + 0,
+                goods::cotton(val) => val + 0,
+                goods::gold(val) => val + 0,
+                goods::iron(val) => val + 0,
+            }
+        }
+        pub fn swap(&self, tofill: &mut Vec<goods>, index: usize, vax: u64) {
+            tofill.remove(index);
+
+            let x = match &self {
+                goods::sugar(val) => goods::sugar(vax),
+                goods::ice(val) => goods::ice(vax),
+                goods::cotton(val) => goods::cotton(vax),
+                goods::gold(val) => goods::gold(vax),
+                goods::iron(val) => goods::iron(vax),
+            };
+            tofill.push(x);
+        }
+    }
+
+    impl port {
+        pub fn update(&mut self, manifest: &mut [goods], buy: bool) {
+            for i in manifest {
+                for j in 0..self.economy.len() - 1 {
+                    if i.same(&self.economy[j]) {
+                        println!(
+                            "{:?}{:?}{:?}",
+                            i,
+                            &self.economy[j],
+                            i.same(&self.economy[j])
+                        );
+                        let v;
+                        if buy {
+                            v = &self.economy[j].getVal() + (i.getVal() / 5);
+                            i.swap(&mut self.economy, j, v);
+                        } else {
+                            v = &self.economy[j].getVal() + (i.getVal() / 5);
+                            i.swap(&mut self.economy, j, v);
+                        }
+                        println!(
+                            "{:?}{:?}{:?}",
+                            i,
+                            &self.economy[&self.economy.len() - 1],
+                            i.same(&self.economy[&self.economy.len() - 1])
+                        );
+                    }
+                }
+            }
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
-#[test]
+    #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,91 @@
-#[macro_use]                                                                 
-extern crate serde_derive;                                                   
+#[macro_use]
+extern crate serde_derive;
 
-extern crate serde;                                                          
-extern crate serde_json; 
+extern crate serde;
+extern crate serde_json;
 mod econosim;
 use econosim::start_econ::*;
 
 fn main() {
-    for i in bootstrap("test.txt".to_string()){
+    let mut places = bootstrap("test.txt".to_string());
+    for i in &places {
         println!("{:?}", i);
+    }
+    let mut player: Ship = Ship {
+        name: "Black Pearl".to_string(),
+        sizeof: btype::large,
+        hold: vec![
+            goods::iron(1),
+            goods::sugar(100),
+            goods::gold(34),
+            goods::cotton(50),
+        ],
+        visited: vec![0, 1, 2, 1, 2, 1, 0, 1, 0],
+        crew: vec![Sailor {
+            Name: "Jack".to_string(),
+            Health: Health {
+                Wounds: vec![wound::arm(10), wound::leg(10)],
+            },
+        }],
+    };
+    println!("\n{:?}", player);
+    let Bob = Sailor {
+        Name: "Bob".to_string(),
+        Health: Health {
+            Wounds: vec![wound::arm(10), wound::leg(25)],
+        },
+    };
+    let x: u64 = Bob.Health.total();
+    println!("{}", x);
+    places[0].update(player.hold.as_mut_slice(), true);
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Ship {
+    pub name: String,
+    pub sizeof: btype,
+    pub hold: Vec<goods>,
+    pub visited: Vec<usize>,
+    pub crew: Vec<Sailor>,
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Sailor {
+    pub Name: String,
+    pub Health: Health,
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Health {
+    pub Wounds: Vec<wound>,
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub enum wound {
+    arm(u64),
+    hand(u64),
+    leg(u64),
+    chest(u64),
+    vitals(u64),
+    eye(u64),
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub enum btype {
+    large,
+    meduim,
+    small,
+}
+
+impl Health {
+    fn total(&self) -> u64 {
+        let mut health = 0;
+        for i in &self.Wounds {
+            health = match i {
+                wound::arm(value) => value + health,
+                wound::leg(value) => value + health,
+                wound::hand(value) => value + health,
+                wound::chest(value) => value + health,
+                wound::vitals(value) => value + health,
+                wound::eye(value) => value + health,
+                _ => 0 + health,
+            };
+        }
+        health
     }
 }
